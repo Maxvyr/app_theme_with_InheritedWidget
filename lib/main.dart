088a9b4b43125346a12c93f161a98a588f91be69
Maxvyr <maxvid30@gmail.com>
2021-10-09 12:47:48 +0200

    sparkles: add riverpod + bt change mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tengu_sensei_theme/tengu_sensei_theme.dart';
 import 'package:tengu_sensei_theme/theme/app_theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
-class MyApp extends StatelessWidget {
+final colorThemeProvider =
+    StateProvider<AppColorTheme>((ref) => AppColorTheme.auto);
+// AppColorTheme colorTheme = AppColorTheme.auto;
+
+class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return MaterialApp(
       title: 'Flutter Demo',
-      builder: (context, child) => AppDynamicTheme(child: child!),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      builder: (context, child) => AppDynamicTheme(
+        child: child!,
+        colorTheme: ref.watch(colorThemeProvider).state,
+      ),
+      home: Builder(builder: (context) {
+        final theme = AppTheme.of(context);
+        return Container(
+          color: theme.color.background1,
+          child: const MyHomePage(),
+        );
+      }),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
@@ -32,12 +48,48 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     final theme = AppTheme.of(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-          child: Wrap(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        const ColorGallery(),
+        Consumer(builder: (context, ref, child) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  debugPrint(ref.read(colorThemeProvider).state.toString());
+                  ref.read(colorThemeProvider).state = AppColorTheme.light;
+                  debugPrint(ref.watch(colorThemeProvider).state.toString());
+                },
+                child: const Text("Light"),
+              ),
+              ElevatedButton(
+                onPressed: () =>
+                    ref.read(colorThemeProvider).state = AppColorTheme.auto,
+                child: const Text("Auto"),
+              ),
+              ElevatedButton(
+                onPressed: () =>
+                    ref.read(colorThemeProvider).state = AppColorTheme.dark,
+                child: const Text("Dark"),
+              ),
+            ],
+          );
+        }),
+      ],
+    );
+  }
+}
+
+class ColorGallery extends StatelessWidget {
+  const ColorGallery({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = AppTheme.of(context);
+    return Center(
+      child: Wrap(
         spacing: 50.0,
         children: [
           ...[
@@ -49,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
             (x) => _ColorTile(color: x),
           ),
         ],
-      )),
+      ),
     );
   }
 }
@@ -64,6 +116,7 @@ class _ColorTile extends StatelessWidget {
     return Container(
       decoration: BoxDecoration(
         color: color,
+        border: Border.all(),
       ),
       width: 80.0,
       height: 80.0,
